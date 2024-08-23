40c88d873246eca965d59f76fdf9fc58648807c4
oelbert <oliver.elbert36@gmail.com>
2024-08-23 12:50:31 -0400

    adding periodic data handling

--- tests/serialized_test_data_generation/serialbox_to_netcdf.py
+++ tests/serialized_test_data_generation/serialbox_to_netcdf.py
@@ -6,7 +6,7 @@ import xarray as xr
 import f90nml
 import numpy as np
 
-sys.path.append("/serialbox/python")  # noqa: E402
+sys.path.append("/autofs/ncrc-svm1_home2/Oliver.Elbert/code/repos/serialbox/install/python/")  # noqa: E402
 import serialbox  # noqa: E402
 
 
@@ -52,10 +52,14 @@ def main(data_path: str, output_path: str):
     if namelist_filename_out != namelist_filename_in:
         shutil.copyfile(os.path.join(data_path, "input.nml"), namelist_filename_out)
     namelist = f90nml.read(namelist_filename_out)
-    total_ranks = (
-        6 * namelist["fv_core_nml"]["layout"][0] * namelist["fv_core_nml"]["layout"][1]
-    )
-
+    if namelist["fv_core_nml"]["grid_type"] <= 3:
+        total_ranks = (
+            6 * namelist["fv_core_nml"]["layout"][0] * namelist["fv_core_nml"]["layout"][1]
+        )
+    else:
+        total_ranks = (
+            namelist["fv_core_nml"]["layout"][0] * namelist["fv_core_nml"]["layout"][1]
+        )
     savepoint_names = get_all_savepoint_names(data_path)
     for savepoint_name in sorted(list(savepoint_names)):
         rank_list = []
@@ -64,11 +68,14 @@ def main(data_path: str, output_path: str):
         names_list = list(
             serializer.fields_at_savepoint(serializer.get_savepoint(savepoint_name)[0])
         )
+        print(f"Reading from savepoint {savepoint_name}")
         serializer_list = []
         for rank in range(total_ranks):
+            print(f"Reading rank {rank}")
             serializer = get_serializer(data_path, rank)
             serializer_list.append(serializer)
             savepoints = serializer.get_savepoint(savepoint_name)
+            print(f"There are {len(savepoints)} savepoints")
             rank_data = {}
             for name in set(names_list):
                 rank_data[name] = []
